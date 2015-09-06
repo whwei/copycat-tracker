22040164fe37692ca124276c0fdd373744b17d9f
whwei <whw.0922@gmail.com>
2015-09-06 19:09:28 +0800

    Dependency

--- src/dependency.js
+++ src/dependency.js
@@ -1,11 +1,22 @@
+var Tracker = require('./tracker')
 
 // Dependency
 function Dependency() {
-
+  this.dependents = {}
 }
 
 Dependency.prototype.depend = function() {
+  var computation = Tracker.currentComputation
 
+  if (!computation) return
+
+  if (!this.dependents[computation.id]) {
+    this.dependents[computation.id] = computation
+
+    computation.onInvalidate(function() {
+      delete this.dependents[computation.id]
+    }.bind(this))
+  }
 }
 
 Dependency.prototype.changed = function() {
